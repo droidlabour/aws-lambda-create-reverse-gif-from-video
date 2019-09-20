c649768f7ca5f179ef810aa8ca3acc095205b0e3
Joy Prakash Sharma <droidlabour@gmail.com>
2019-09-20 19:49:11 +0530

    Concat gif with itself in reverse order

--- index.js
+++ index.js
@@ -1,15 +1,8 @@
 var ffmpeg = require('fluent-ffmpeg');
 
-/*
-ffmpeg.getAvailableFormats(function(err, formats) {
-  console.log('Available formats:');
-  console.dir(formats);
-});
-*/
-
 ffmpeg()
 	.input('/Users/droidlabour/Downloads/10-second-video-fail-452824.mp4')
-	.seekInput(7)
+	.seekInput(2)
 	.inputOptions('-t 2')
 	.addOutputOption('-f gif')
 	.on('start', function(cmd) {
@@ -17,9 +10,28 @@ ffmpeg()
 	})
 	.on('end', function() {
 		console.log('GIF created');
+		reverse.run();
 	})
 	.on('error', function(error) {
 		console.log('ERROR creating gif: ' + error);
 	})
-	.output('/Users/droidlabour/Downloads/2.gif')
+	.output('/Users/droidlabour/Downloads/1.gif')
 	.run();
+
+// Convert video to gif & concat with itself in reverse order
+// https://stackoverflow.com/questions/42257354/concat-a-video-with-itself-but-in-reverse-using-ffmpeg#comment79872823_42257863
+// ffmpeg -i 2.gif -filter_complex "[0:v]reverse,fifo[r];[0:v][r] concat=n=2:v=1 [v]" -map "[v]" merge.gif
+
+var reverse = ffmpeg()
+	.input('/Users/droidlabour/Downloads/1.gif')
+	.complexFilter('[0:v]reverse,fifo[r];[0:v][r] concat=n=2:v=1 [v]', ['v'])
+	.on('start', function(cmd) {
+		console.log('Reversing gif:\n\t' + cmd);
+	})
+	.on('end', function() {
+		console.log('GIF reversed');
+	})
+	.on('error', function(error) {
+		console.log('ERROR reversing gif: ' + error);
+	})
+	.output('/Users/droidlabour/Downloads/reverse.gif');
